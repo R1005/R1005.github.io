4bb04ef71c648bc6f9310f7571ae1c63260bb679
R1005 <63446258+R1005@users.noreply.github.com>
2021-01-23 19:41:50 +0900

    Update motions.js

--- motions.js
+++ motions.js
@@ -18,3 +18,28 @@ function sendMsg() {
     }
 
 }
+
+var currentItemNum = 1;
+var $slideContainer = $('.slider__container');
+var slideItemNum = $('.slider__item').length;
+var slideItemWidth = $('.slider__item').innerWidth();
+var slideContainerWidth = slideItemWidth * slideItemNum;
+var DURATION = 1000;
+
+$slideContainer.attr('style', 'width:' + slideContainerWidth + 'px');
+
+function autoSlide(){
+
+    if(currentItemNum < slideItemNum){
+
+        $slideContainer.animate({left: '-='+slideItemWidth+'px'}, DURATION);
+        currentItemNum++;
+
+    }else if(currentItemNum = slideItemNum){
+
+        $slideContainer.css('left', '+='+slideItemWidth*(slideItemNum-1)+'px');
+        currentItemNum = 1;//現在のスライド番号を１に戻す
+    }
+}
+
+setInterval(autoSlide, 5000);
