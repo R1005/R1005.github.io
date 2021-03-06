accf37142aa13a8352c8f444e68988e6190be3f5
R1005 <rinhana2004@gmail.com>
2021-03-06 15:37:28 +0900

    changed

--- motions.js
+++ motions.js
@@ -1,30 +1,5 @@
-$(function(){
-    
-    var ua = navigator.userAgent;
-    if(ua.indexOf("iPhone") > 0 || ua.indexOf("Android") > 0 && ua.indexOf("Mobile") > 0){
-    console.log("スマートフォン用のコードを書く");
-    }else{
-    console.log("PC/タブレット用のコードを書く");
-    }
-  });
-
-  <p id = "result"></p>
-
-
-  // id属性で要素を取得
-var textbox_element = document.getElementById('textbox');
-
-// 新しいHTML要素を作成
-var new_element = document.createElement('p');
-new_element.textContent = '追加テキスト';
-
-// 指定した要素の中の末尾に挿入
-textbox_element.appendChild(new_element);
-
-
 function getAccessWay() {
-  var ta = document.getElementById('aff')
-  var msg = ""
+
   var ut = navigator.userAgent;
   
   if(ut.indexOf('iPhone') > 0 || ut.indexOf('iPod') > 0 || ut.indexOf('Android') > 0 && ut.indexOf('Mobile') > 0){
@@ -36,6 +11,5 @@ function getAccessWay() {
   else{
     alert("PC");
   }
-  
-  // ta.textContent = msg
+
 }
