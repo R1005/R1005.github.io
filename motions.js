be4c1652410fb46dfc216372b8afdc1a62bfd8d9
R1005 <rinhana2004@gmail.com>
2021-03-06 15:58:41 +0900

    trial

--- motions.js
+++ motions.js
@@ -1,5 +1,14 @@
 function getAccessWay() {
 
+  var textbox_element = document.getElementById('aff');
+
+// 新しいHTML要素を作成
+  var new_element = document.createElement('div');
+  new_element.textContent = '<div id = "aff"><div class = "aff_left"><a href = "pc.moppy.jp/entry/invite.php?invite=T63de169&type=service" target = "_blank" rel = "noopener noreferrer"><img src = "img.moppy.jp/pub/pc/friend/120x600-11.jpg" class = "aff_img"></a></div><div class = "aff_right"><a href = "pc.moppy.jp/entry/invite.php?invite=T63de169&type=service" target = "_blank"><img src = "img.moppy.jp/pub/pc/friend/120x600-3.jpg" class = "aff_img"></a></div></div>';
+
+// 指定した要素の中の末尾に挿入
+  textbox_element.appendChild(new_element);
+
   var ut = navigator.userAgent;
   
   if(ut.indexOf('iPhone') > 0 || ut.indexOf('iPod') > 0 || ut.indexOf('Android') > 0 && ut.indexOf('Mobile') > 0){
